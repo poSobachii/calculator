43aa981427d3088e6234d1fb23a3895950a67baa
poSobachii <grishanov@inbox.lv>
2018-08-24 14:59:19 +0300

    home test

--- src/calculator/Calculator.java
+++ src/calculator/Calculator.java
@@ -13,45 +13,19 @@ class Calculator {
 
         double a = Double.parseDouble(expression[0]);
         double b = Double.parseDouble(expression[2]);
-        double result;
-        double resultx;
-        switch (expression[1]) {
-            case "+":
-                result = a + b;
-                break;
-            case "-":
-                result = a - b;
-                break;
-            case "/":
-                result = a / b;
-                break;
-            case "*":
-                result = a * b;
-                break;
-            default:
-                return "ERROR";
-        }
-        if (expression.length <4){
-            return String.valueOf(result);
-        }
         double c = Double.parseDouble(expression[4]);
-        switch (expression[3]) {
-            case "+":
-                resultx = result + c;
+        double result;
+        switch (expression[1] + expression[3]) {
+            case "+ *":
+                result = a + (b * c);
                 break;
-            case "-":
-                resultx = result - c;
-                break;
-            case "/":
-                resultx = result / c;
-                break;
-            case "*":
-                resultx = result * c;
+            case "+ -":
+                result = a + b - c;
                 break;
             default:
                 return "ERROR";
         }
-        return String.valueOf(resultx);
+        return String.valueOf(result);
     }
 
 }
