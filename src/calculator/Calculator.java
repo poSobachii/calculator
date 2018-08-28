0c4b1f22d7674b6275e140cc550d9dff8a023cd9
Alex <grishanov@inbox.lv>
2018-08-28 18:43:09 +0300

    second java lesson

--- src/calculator/Calculator.java
+++ src/calculator/Calculator.java
@@ -1,31 +1,25 @@
 package calculator;
 
-import java.sql.SQLOutput;
-
-/**
- * @author Dimitrijs Fedotovs <a href="http://www.bug.guru">www.bug.guru</a>
- * @version 1.0
- * @since 1.0
- */
 class Calculator {
 
     String calculate(String[] expression) {
+        double result = Double.parseDouble(expression[0]);
+        for (int i = 1; i < expression.length; i += 2) {
+            String op = expression[i];
+            double b = Double.parseDouble(expression[i + 1]);
+            switch (op) {
+                case "+":
+                    result += b;
+                    break;
+                case "-":
+                    result -= b;
+                    break;
+                default:
+                    return "ERROR";
+            }
 
-        double a = Double.parseDouble(expression[0]);
-        double b = Double.parseDouble(expression[2]);
-        double c = Double.parseDouble(expression[4]);
-        double result;
-        switch (expression[1] + expression[3]) {
-            case "+ *":
-                result = a + (b * c);
-                break;
-            case "+ -":
-                result = a + b - c;
-                break;
-            default:
-                return "ERROR";
         }
+
         return String.valueOf(result);
     }
-
 }
