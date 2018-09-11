82e032c7b8397be740c8d61ab0012852092b1ebf
Alex <grishanov@inbox.lv>
2018-09-11 19:25:01 +0300

    second java lesson calc finished ?

--- src/calculator/Calculator.java
+++ src/calculator/Calculator.java
@@ -6,11 +6,36 @@ import java.util.List;
 class Calculator {
 
     String calculate(String[] expression) {
+        List<String> tmp = new ArrayList<>(List.of(expression));
+
+        int brOpenIndex = -1;
+        do {
+            brOpenIndex = -1;
+            for (int i = 0; i < tmp.size(); i++) {
+                String a = tmp.get(i);
+                if (a.equals("(")) {
+                    brOpenIndex = i;
+                } else if (a.equals(")")) {
+                    var inBr = tmp.subList(brOpenIndex + 1, i);
+                    var result = calculate(inBr);
+                    tmp.subList(brOpenIndex, i + 1).clear();
+                    tmp.add(brOpenIndex, result);
+                    break;
+                }
+
+            }
+        }
+        while (brOpenIndex != -1);
+
+        return calculate(tmp);
+    }
+
+    String calculate(List<String> expression) {
         List<String> tmp = new ArrayList<>();
-        double a = Double.parseDouble(expression[0]);
-        for (int i = 1; i < expression.length; i += 2) {
-            String op = expression[i];
-            double b = Double.parseDouble(expression[i + 1]);
+        double a = Double.parseDouble(expression.get(0));
+        for (int i = 1; i < expression.size(); i += 2) {
+            String op = expression.get(i);
+            double b = Double.parseDouble(expression.get(i + 1));
             switch (op) {
                 case "+":
                 case "-":
